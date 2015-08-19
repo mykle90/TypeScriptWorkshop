4246b98f8b4313282c84d1dcc79bcc1f8ebadf1d
mykle90 <mykle90@gmail.com>
2015-08-19 10:14:54 +0200

    Removed solution

--- Bekk Workshop/Scripts/Typescript/App.ts
+++ Bekk Workshop/Scripts/Typescript/App.ts
@@ -7,12 +7,6 @@ class App {
 
     constructor() {
         this.shapes = [];
-        this.shapes.push(new Shapes.Square(5));
-        this.shapes.push(new Shapes.Rectangle(5, 3));
-        this.shapes.push(new Shapes.Shape());
-        this.shapes.push(new Shapes.Circle(7));
-        this.shapes.push(new Shapes.Sphere(6));
-        this.shapes.push(Shapes.Sphere.oneSizeBall());
     }
 
     getShape(index: number): Shapes.IShape {
@@ -26,8 +20,7 @@ class App {
 
 let run = () =>  {
     const app = new App();
-    $("body").append(app.getShape(3).toString());
-    $("body").append("<br /> " + app.getShape(4).toString());
+    
 }
 
 window.onload = () => {
