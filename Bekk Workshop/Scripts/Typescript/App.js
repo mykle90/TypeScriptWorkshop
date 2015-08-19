8c17865563127dce3abbf3e77e8ac5f2cbe527c2
mykle90 <mykle90@gmail.com>
2015-08-19 10:21:39 +0200

    Removed solution from master

--- Bekk Workshop/Scripts/Typescript/App.js
+++ Bekk Workshop/Scripts/Typescript/App.js
@@ -2,12 +2,6 @@
 var App = (function () {
     function App() {
         this.shapes = [];
-        this.shapes.push(new Shapes.Square(5));
-        this.shapes.push(new Shapes.Rectangle(5, 3));
-        this.shapes.push(new Shapes.Shape());
-        this.shapes.push(new Shapes.Circle(7));
-        this.shapes.push(new Shapes.Sphere(6));
-        this.shapes.push(Shapes.Sphere.oneSizeBall());
     }
     App.prototype.getShape = function (index) {
         if (index > this.shapes.length || index < 0) {
@@ -19,8 +13,6 @@ var App = (function () {
 })();
 var run = function () {
     var app = new App();
-    $("body").append(app.getShape(3).toString());
-    $("body").append("<br /> " + app.getShape(4).toString());
 };
 window.onload = function () {
     run();
